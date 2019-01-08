45883499348ce4754f49f80ced1d7cf07e0a4c4f
klishevich <mklishevich@alfabank.ru>
2019-01-08 13:33:14 +0300

    feat(greetings): with js and with ts

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,11 +1,23 @@
 import * as React from "react";
 
+import { greeter } from "../utils/greetings";
+import { greeterTs, Student } from "../utils/greetingsTs";
+
 export interface HelloProps { compiler: string; framework: string; }
 
 // 'HelloProps' describes the shape of props.
 // State is never set so we use the '{}' type.
 export class Hello extends React.Component<HelloProps, {}> {
     render() {
-        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+        return (
+            <div>
+                <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>
+                <div><b>Greeter</b></div>
+                <div>{ greeter("Michael") }</div>
+                <div><b>Greeter TS</b></div>
+                <div>{ greeterTs(new Student('Michael', 'Alexandr', 'Student')) }</div>
+                <div>{ greeterTs({ firstName: 'Michael', lastName: 'Klishevich' }, 27) }</div>
+            </div>
+        );
     }
 }
---
+++ src/utils/greetingsTs.ts
@@ -0,0 +1,19 @@
+export class Student {
+    fullName: string;
+    constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+export function greeterTs(person: Person, age?: number) {
+    let res = `TS Hello ${person.firstName} ${person.lastName}`;
+    if (age) {
+        res += `. You are ${age} y.o.`
+    }
+    return res
+}
